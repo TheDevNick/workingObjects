e29ad1509818b0360d6e037e7512221906795ec6
Nick Hollins <nholl22@wgu.edu>
2021-05-24 18:10:36 -0400

    working with objects

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// creating a pen object
+const pen = {
+    type: 'ballpoint',
+    color: 'blue',
+    brand: 'Bic'
+};
+
+// accessing the pen object properties
+console.log(pen.type);
+console.log(pen.brand);
+console.log(pen.color);
+
+//  changing the value of the pen color property
+pen.color = 'red';
+console.log(pen.color);
+
+
